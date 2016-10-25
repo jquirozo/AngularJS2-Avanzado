97bbcc8114bec10a526401c3ad9d96fe7c104af8
Jacobo <jquirozo@outlook.com>
2016-10-25 12:38:48 -0500

    Angular Modules I
    se crea modulo adicional del modulo inicial. Se evita asi errores de
    conflictos en directivas por ejemplo HighlightDirective.

---
+++ app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule  } from '@angular/platform-browser';
+
+/* App Root */
+import { AppComponent }   from './app.component';
+import { HighlightDirective }   from './highlight.directive';
+import { TitleComponent } from './title.component';
+import { UserService } from './user.service';
+
+/* Contact Imports */
+import { ContactModule }      from './contact/contact.module';
+
+@NgModule({
+  imports:      [ BrowserModule, ContactModule  ],
+  declarations: [ 	AppComponent,
+					HighlightDirective,
+					TitleComponent					
+				],
+  bootstrap:    [ AppComponent ],
+  providers: [ UserService ],
+})
+
+export class AppModule { }
